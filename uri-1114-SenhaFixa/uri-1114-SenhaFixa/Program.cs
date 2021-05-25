29edf045df07bd9e1ef319f86828a27c3b2c52db
Nicolas Moreira Zanatta <nicolas.mzanatta93@gmail.com>
2021-05-25 15:40:38 -0300

    uri

---
+++ uri-1114-SenhaFixa/uri-1114-SenhaFixa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace uri_1114_SenhaFixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
